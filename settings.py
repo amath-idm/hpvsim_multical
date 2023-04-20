8d3e3aa3104ab6d524989676dc89f49c488aef79
Robyn Stuart <robyna.s@gmail.com>
2023-04-20 15:48:14 +1000

    add settings

---
+++ settings.py
@@ -0,0 +1,39 @@
+'''
+Settings
+'''
+
+# Comment out locations to not run
+locations = [
+    'angola',       # 0
+    'benin',        # 1
+    'burkina faso', # 2
+    'burundi',      # 3
+    'cameroon',     # 4
+    'chad',         # 5
+    'cote divoire', # 6
+    'drc',          # 7
+    'ethiopia',     # 8
+    'ghana',        # 9
+    'guinea',       # 10
+    'kenya',        # 11
+    'madagascar',   # 12
+    'malawi',       # 13
+    'mali',         # 14
+    'mozambique',   # 15
+    'niger',        # 16
+    'nigeria',      # 17
+    'rwanda',       # 18
+    'senegal',      # 19
+    'sierra leone', # 20
+    'somalia',      # 21
+    'south africa', # 22
+    'south sudan',  # 23
+    'sudan',        # 24
+    'tanzania',     # 25
+    'togo',         # 26
+    'uganda',       # 27
+    'zambia',       # 28
+    'zimbabwe',     # 29
+]
+
+nosbdata_locations = ["cote d'ivoire", "cote divoire", "somalia", "south sudan", "sudan"]
