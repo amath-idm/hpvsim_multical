73311162af337c237184e918b9604ff30e2c15a2
Robyn Stuart <robyna.s@gmail.com>
2023-04-26 16:34:21 +1000

    change priors again

--- run_singlecal.py
+++ run_singlecal.py
@@ -42,7 +42,7 @@ def run_calib(location=None, n_trials=None, n_workers=None,
 
     genotype_pars = dict(
         hpv16=dict(
-            transform_prob=[8 / 1e10, 5 / 1e10, 10 / 1e10],
+            transform_prob=[8 / 1e10, 6 / 1e10, 10 / 1e10],
             sev_fn=dict(
                 k = [0.2,0.15,0.25],
             ),
@@ -54,7 +54,7 @@ def run_calib(location=None, n_trials=None, n_workers=None,
             ),
         ),
         hrhpv=dict(
-            transform_prob=[2 / 1e10, 1 / 1e10, 3 / 1e10],
+            transform_prob=[1 / 1e10, .5 / 1e10, 1.5 / 1e10],
             sev_fn=dict(
                 k = [0.2,0.15,0.25],
             ),
