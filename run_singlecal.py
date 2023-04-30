1d6b4023fcf21a1f677f1b3599898ddc420a8849
Robyn Stuart <robyna.s@gmail.com>
2023-05-01 07:53:26 +1000

    even more

--- run_singlecal.py
+++ run_singlecal.py
@@ -32,7 +32,7 @@ storage     = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug] # Storage
 def make_priors(location):
     all_genotype_pars = dict(
         ethiopia = dict(
-            hpv16=dict(transform_prob=[8e-10, 6e-10, 10e-10]),
+            hpv16=dict(transform_prob=[10e-10, 8e-10, 12e-10]),
             hpv18=dict(transform_prob=[3e-10, 2e-10, 5e-10]),
             hrhpv=dict(
                 transform_prob=[3e-10, 2e-10, 5e-10],
