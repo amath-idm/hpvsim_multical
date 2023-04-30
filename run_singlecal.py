fe2f5e80114b4cb6267a701014e2842e8d3ba35f
Robyn Stuart <robyna.s@gmail.com>
2023-05-01 07:18:17 +1000

    fix upper

--- run_singlecal.py
+++ run_singlecal.py
@@ -43,7 +43,7 @@ def make_priors(location):
             hpv16=dict(transform_prob=[6e-10, 4e-10, 8e-10]),
             hpv18=dict(transform_prob=[3e-10, 2e-10, 5e-10]),
             hrhpv=dict(
-                transform_prob=[3e-10, 2e-10, 5e-11],
+                transform_prob=[3e-10, 2e-10, 5e-10],
                 sev_fn=dict(k=[0.15, 0.10, 0.2])
             ),
         )
