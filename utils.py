7e3d007bf966e97fa63b49e6cd5a7f8db56606bf
Robyn Stuart <robyna.s@gmail.com>
2023-05-19 15:22:25 +1000

    add to targets

--- utils.py
+++ utils.py
@@ -129,8 +129,8 @@ def make_datafiles(locations):
             f'data/{dflocation}_asr_cancer_incidence.csv',
         ]
 
-        # if location in cancer_type_locs:
-        #     datafiles[location] += [f'data/{dflocation}_cancer_types.csv']
+        if location in cancer_type_locs:
+            datafiles[location] += [f'data/{dflocation}_cancer_types.csv']
         # if location in cin3_type_locs:
         #     datafiles[location] += [f'data/{dflocation}_cin3_types.csv']
         # if location in cin1_type_locs:
