eea0c5c8f4823df8866b134f4e7d35025f3cbf57
Robyn Stuart <robyna.s@gmail.com>
2023-10-09 10:21:21 +1100

    replot sb

--- read_sbdata.py
+++ read_sbdata.py
@@ -442,7 +442,7 @@ if __name__ == '__main__':
     # countries, dff, df2, rvs = read_debut_data(dist_type=dist_type)
 
     dist_type = 'lognormal'
-    do_run = False
+    do_run = True
 
     if do_run:
         sims, afs_df, pm_df, agediff_df, casual_df = get_sb_from_sims(
