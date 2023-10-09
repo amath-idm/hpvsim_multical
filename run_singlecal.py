5fea974a0838e671e21cee9aa9eb2454ed9f5028
Robyn Stuart <robyna.s@gmail.com>
2023-10-10 08:10:12 +1100

    retrying with new pars

--- run_singlecal.py
+++ run_singlecal.py
@@ -44,38 +44,26 @@ storage = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug]  # Storage fo
 ########################################################################
 def make_priors(add_1618=True):
     default = dict(
-        hi5=dict(
-            dur_precin=dict(par1=[4, 3, 7, 0.5], par2=[20, 15, 25, 1]),
-            dur_cin=dict(par1=[12, 10, 15, 0.5], par2=[30, 20, 35, 1]),
-            cin_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            cancer_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            rel_beta=[0.75, 0.7, 1.25, 0.05]
+        dur_precin=dict(par1=[2, 2, 5, 0.1], par2=[10, 10, 30, 0.5]),
+        dur_cin=dict(par1=[4, 2, 15, 0.1], par2=[10, 10, 30, 0.5]),
+        cancer_fn=dict(
+            k=[0.15, 0.1, 0.6, 0.01],
+            x_infl=[10, 7, 15, 1],
+            y_max=[1, 0.4, 1, 0.1],
+
         ),
-        ohr=dict(
-            dur_precin=dict(par1=[4, 3, 7, 0.5], par2=[20, 15, 25, 1]),
-            dur_cin=dict(par1=[12, 10, 15, 0.5], par2=[30, 20, 35, 1]),
-            cin_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            cancer_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            rel_beta=[0.75, 0.7, 1.25, 0.05]
-        ),
+        rel_beta=[0.75, 0.7, 1.25, 0.05]
+    )
+
+    genotype_pars = dict(
+        hi5=sc.dcp(default),
+        ohr=sc.dcp(default)
     )
 
     if add_1618:
-        default['hpv16'] = dict(
-            dur_precin=dict(par1=[4, 3, 7, 0.5], par2=[20, 15, 25, 1]),
-            dur_cin=dict(par1=[12, 10, 15, 0.5], par2=[30, 20, 35, 1]),
-            cin_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            cancer_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-        )
-        default['hpv18'] = dict(
-            dur_precin=dict(par1=[4, 3, 7, 0.5], par2=[20, 15, 25, 1]),
-            dur_cin=dict(par1=[12, 10, 15, 0.5], par2=[30, 20, 35, 1]),
-            cin_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            cancer_fn=dict(k=[0.3, 0.1, 0.5, 0.01]),
-            rel_beta=[0.75, 0.7, 0.95, 0.05]
-        )
-
-    genotype_pars = sc.dcp(default)
+        genotype_pars['hpv16'] = sc.dcp(default)
+        genotype_pars['hpv18'] = sc.dcp(default)
+        del genotype_pars['hpv16']['rel_beta']
 
     return genotype_pars
 
@@ -89,10 +77,10 @@ def run_calib(location=None, n_trials=None, n_workers=None,
     # Define the calibration parameters
     calib_pars = dict(
         beta=[0.2, 0.1, 0.3, 0.02],
-        cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
-        cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
-        cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
-        cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
+        # cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
+        # cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
+        # cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
+        # cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
         # sev_dist=dict(par1=[1.0, 0.75, 1.5, 0.05])
     )
 
