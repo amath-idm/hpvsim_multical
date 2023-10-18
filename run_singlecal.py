7420105c65840d97e9945a19a888f0db6da98dd9
Robyn Stuart <robyna.s@gmail.com>
2023-10-19 10:43:02 +1100

    cdi

--- run_singlecal.py
+++ run_singlecal.py
@@ -138,7 +138,7 @@ def load_calib(location=None, do_plot=True, which_pars=0, save_pars=True, filest
 if __name__ == '__main__':
 
     T = sc.timer()
-    locations = ['ethiopia']
+    locations = ['cote divoire']
     filestem = '_oct16'
 
     if cal_type == 'immunovarying':
