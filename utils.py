643144e053e5dfead8ff032f22d6737117f993f9
Robyn Stuart <robyna.s@gmail.com>
2023-10-20 09:28:34 +1100

    reverse ordering

--- utils.py
+++ utils.py
@@ -157,7 +157,7 @@ def make_layer_probs(location=None, marriage_scale=1):
     if location == 'cote divoire':
         layer_probs['c'][1] = np.array([
             # 0, 5,  10,  15,  20,  25,  30,  35,  40,  45,  50,  55,  60,  65,   70,   75
-            0,   0, 0.1, 0.6, 0.6, 0.6, 0.5, 0.4, 0.35, 0.35, 0.25, 0.1, 0.05, 0.05, 0.05, 0.05
+            0,   0, 0.1, 0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.5, 0.3, 0.1, 0.05, 0.05, 0.05, 0.05
         ])
     if location == 'drc':
         layer_probs['c'][1] = np.array([
