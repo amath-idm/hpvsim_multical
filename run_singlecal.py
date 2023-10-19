9786866e4cfb180fcd54d82691b3f05738cc1f39
Robyn Stuart <robyna.s@gmail.com>
2023-10-19 14:42:52 +1100

    cdi again

--- run_singlecal.py
+++ run_singlecal.py
@@ -78,10 +78,10 @@ def run_calib(location=None, n_trials=None, n_workers=None,
     # Define the calibration parameters
     calib_pars = dict(
         beta=[0.2, 0.1, 0.3, 0.02],
-        # cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
-        # cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
-        # cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
-        # cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
+        cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
+        cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
+        cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
+        cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
         # sev_dist=dict(par1=[1.0, 0.75, 1.5, 0.05])
     )
 
