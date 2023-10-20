2e9a31c9ede276c37227c5a804c953acfe7aa24d
Robyn Stuart <robyna.s@gmail.com>
2023-10-20 15:28:20 +1100

    kenya 5

--- run_singlecal.py
+++ run_singlecal.py
@@ -82,14 +82,13 @@ def run_calib(location=None, n_trials=None, n_workers=None,
     # Define the calibration parameters
     calib_pars = dict(
         beta=[0.2, 0.1, 0.3, 0.02],
+        # kenya?
+        cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
+        cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
+        cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
+        cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
         # sev_dist=dict(par2=[0.2, 0.15, 0.25, 0.05])
     )
-    if location=='kenya':
-        calib_pars['cross_imm_sus_med']=[0.3, 0.2, 0.6, 0.05],
-        calib_pars['cross_imm_sus_high']=[0.5, 0.3, 0.7, 0.05],
-        calib_pars['cross_imm_sev_med']=[0.5, 0.3, 0.7, 0.05],
-        calib_pars['cross_imm_sev_high']=[0.7, 0.5, 0.9, 0.05],
-
 
     if mc_gpars is None: add_1618 = True
     else: add_1618 = False
