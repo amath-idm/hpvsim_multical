6c79a3f4ba6647c94ac389883057e529a16bae66
Robyn Stuart <robyna.s@gmail.com>
2023-10-23 15:40:48 +1100

    zambia

--- utils.py
+++ utils.py
@@ -104,9 +104,11 @@ def make_layer_probs(location=None, marriage_scale=1):
         layer_probs['m'][1]*=.6
     if location in ['guinea', 'nigeria', 'senegal', 'sierra leone', 'sudan']:
         layer_probs['m'][1]*=.7
-    if location in ['uganda', 'zambia', 'zimbabwe']:
+    if location in ['zambia']:
+        layer_probs['m'][1]*=.9
+    if location in ['uganda', 'zimbabwe']:
         layer_probs['m'][1]*=.7
-    if location in ['kenya', 'madagascar', 'malawi', 'mozambique', 'rwanda', 'togo', 'zambia', 'zimbabwe']:
+    if location in ['kenya', 'madagascar', 'malawi', 'mozambique', 'rwanda', 'togo']:
         layer_probs['m'][1]*=.5
         # layer_probs['m'][1][9:]*=.15
     if location=='tanzania':
@@ -259,7 +261,7 @@ def make_layer_probs(location=None, marriage_scale=1):
     if location == 'zambia':
         layer_probs['c'][1] = np.array([
             # 0, 5,  10,  15,  20,  25,  30,  35,  40,  45,  50,  55,  60,  65,   70,   75
-            0,   0, 0.1, 0.6, 0.6, 0.5, 0.4, 0.3, 0.2, 0.05, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01
+            0,   0, 0.1, 0.75, 0.75, 0.5, 0.4, 0.3, 0.2, 0.05, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01
         ])
     if location == 'zimbabwe':
         layer_probs['c'][1] = np.array([
