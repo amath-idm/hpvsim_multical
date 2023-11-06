079fca723408d2b9289cf3de2974aa1df3f874f0
Robyn Stuart <robyna.s@gmail.com>
2023-11-06 20:26:05 +1100

    knde

--- run_singlecal.py
+++ run_singlecal.py
@@ -139,7 +139,7 @@ def load_calib(location=None, do_plot=True, which_pars=0, save_pars=True, filest
 if __name__ == '__main__':
 
     T = sc.timer()
-    locations = ['tanzania']  #loc.locations[20:]
+    locations = ['kenya', 'nigeria', 'drc', 'ethiopia']  #loc.locations[20:]
     filestem = '_nov06'
 
     if cal_type == 'immunovarying':
