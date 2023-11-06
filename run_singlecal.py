cccfbcc70bbcd056a18199b8551b1784d2f5703d
Robyn Stuart <robyna.s@gmail.com>
2023-11-06 19:11:32 +1100

    try casual plot

--- run_singlecal.py
+++ run_singlecal.py
@@ -94,7 +94,7 @@ def run_calib(location=None, n_trials=None, n_workers=None,
                             name=f'{location}_calib_final',
                             datafiles=datafiles,
                             total_trials=n_trials, n_workers=n_workers,
-                            storage=storage
+                            storage=f"{storage}"
                             )
     calib.calibrate()
     filename = f'{location}_calib{filestem}'
