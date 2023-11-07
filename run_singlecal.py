3eebe8440ce1abc08bee467c4dd227077caa3394
Robyn Stuart <robyna.s@gmail.com>
2023-11-08 09:53:56 +1100

    mad1

--- run_singlecal.py
+++ run_singlecal.py
@@ -143,7 +143,7 @@ def load_calib(location=None, do_plot=True, which_pars=0, save_pars=True, filest
 if __name__ == '__main__':
 
     T = sc.timer()
-    locations = ['kenya']  # loc.locations[10:20]
+    locations = ['madagascar']  # loc.locations[10:20]
     filestem = '_nov06'
 
     if cal_type == 'immunovarying':
