787794302b63205dee448e8b0f3150c57b72f1fe
Robyn Stuart <robyna.s@gmail.com>
2023-11-10 08:16:08 +1100

    try mc

--- run_multical.py
+++ run_multical.py
@@ -36,7 +36,7 @@ do_save = True
 
 
 # Run settings for calibration (dependent on debug)
-n_trials    = [5000, 2][debug]  # How many trials to run for calibration
+n_trials    = [7500, 2][debug]  # How many trials to run for calibration
 n_workers   = [40, 1][debug]    # How many cores to use
 storage     = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug] # Storage for calibrations
 
@@ -50,24 +50,22 @@ def make_unique_priors(locations=None):
     for location in locations:
         unique_pars[location] = dict(
             calib_pars = dict(
-                beta=[0.2, 0.1, 0.3, 0.02],
-                cross_imm_sus_med=[0.3, 0.2, 0.6, 0.05],
-                cross_imm_sus_high=[0.5, 0.3, 0.7, 0.05],
-                cross_imm_sev_med=[0.5, 0.3, 0.7, 0.05],
-                cross_imm_sev_high=[0.7, 0.5, 0.9, 0.05],
+                beta=[0.2, 0.1, 0.34, 0.02],
+                m_cross_layer=[0.3, 0.1, 0.7, 0.05],
+                m_partners=dict(
+                    c=dict(par1=[0.2, 0.1, 0.6, 0.02])
+                ),
+                f_cross_layer=[0.1, 0.05, 0.5, 0.05],
+                f_partners=dict(
+                    c=dict(par1=[0.2, 0.1, 0.6, 0.02])
+                ),
             ),
             genotype_pars = dict(
                 hi5=dict(
-                    dur_cin=dict(par1=[4, 2, 6, 0.1], par2=[4, 2, 12, 0.5]),
-                    cin_fn=dict(k=[0.1, 0.05, 0.3, 0.01]),
-                    cancer_fn=dict(ld50=[20, 15, 50, 1]),
-                    rel_beta=[0.75, 0.7, 1., 0.05]
+                    cin_fn=dict(k=[.2, .15, .4, 0.01]),
                 ),
                 ohr=dict(
-                    dur_cin=dict(par1=[4, 2, 6, 0.1], par2=[4, 2, 12, 0.5]),
-                    cin_fn=dict(k=[0.1, 0.05, 0.3, 0.01]),
-                    cancer_fn=dict(ld50=[20, 15, 50, 1]),
-                    rel_beta=[0.75, 0.7, 1., 0.05]
+                    cin_fn=dict(k=[.2, .15, .4, 0.01]),
                 ),
             )
         )
@@ -78,7 +76,7 @@ def make_posterior_df(locations=None, n_results=50):
     dfs = sc.autolist()
     for location in locations:
         dflocation = location.replace(' ', '_')
-        calib = sc.loadobj(f'results/unconstrained/{dflocation}_calib_oct16.obj')
+        calib = sc.loadobj(f'results/unconstrained/{dflocation}_calib_nov06.obj')
         df = sc.dcp(calib.df[:n_results])
         df['location'] = location
         dfs += df
@@ -94,15 +92,10 @@ def run_calib(locations=None, sc_pars=None, n_trials=None, n_workers=None,
     common_pars = dict(
         genotype_pars=dict(
             hpv16=dict(
-                dur_cin=dict(par1=[5, 7, 10, 0.5], par2=[10, 7, 18, 0.5]),
-                cin_fn=dict(k=[0.28, 0.2, 0.4, 0.02]),
-                cancer_fn=dict(ld50=[21, 12, 40, 1]),
+                cin_fn=dict(k=[.35, .25, .45, 0.01]),
             ),
             hpv18=dict(
-                dur_cin=dict(par1=[6.5, 3, 10, 0.5], par2=[9, 4, 19, 0.5]),
-                cin_fn=dict(k=[0.25, 0.1, 0.4, 0.02]),
-                cancer_fn=dict(ld50=[21, 12, 40, 1]),
-                rel_beta=[0.75, 0.7, 1., 0.05]
+                cin_fn=dict(k=[.35, .25, .45, 0.01]),
             )
         )
     )
@@ -124,10 +117,10 @@ def run_calib(locations=None, sc_pars=None, n_trials=None, n_workers=None,
         sims,
         common_pars=common_pars,
         unique_pars=unique_pars,
-        name=f'multical1031',
+        name=f'multical1106',
         datafiles=ut.make_datafiles(locations),
         load_if_exists=True,
-        db_name='multical1031.db',
+        db_name='multical1106.db',
         total_trials=n_trials,
         n_workers=n_workers,
         storage=storage,
@@ -178,41 +171,42 @@ def load_calib(filestem=None, locations=None, do_plot=True, which_pars=0, save_p
 if __name__ == '__main__':
 
     T = sc.timer()
-    filestem = '_oct24'
+    filestem = '_nov06'
     locations = loc.locations
 
     # Run calibration - usually on VMs
     if 'run_calibration' in to_run:
+        sc_pars = None
+        sims, calib = run_calib(locations=locations, sc_pars=sc_pars, n_trials=n_trials, n_workers=n_workers, do_save=do_save, do_plot=False, filestem=filestem)
 
         # Get the parameters from the single cals
         # alldf = make_posterior_df(locations, n_results=1)
-        alldf = sc.loadobj('results/calib_dfs_sc.obj')
-        sc_pars = dict()
-        for location in locations:
-            thisdf = alldf[alldf.location == location]
-            sc_pars[location] = dict(
-                genotype_pars=dict(
-                    hi5=dict(
-                        cin_fn=dict(k=thisdf.hi5_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50),
-                        cancer_fn=dict(ld50=thisdf.hi5_cancer_fn_ld50.iloc[0], method='cin_integral', **dict(k=thisdf.hi5_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50)),
-                        dur_cin=dict(dist='lognormal', par1=thisdf.hi5_dur_cin_par1.iloc[0], par2=thisdf.hi5_dur_cin_par2.iloc[0]),
-                        rel_beta=thisdf.hi5_rel_beta.iloc[0]
-                    ),
-                    ohr=dict(
-                        cin_fn=dict(k=thisdf.ohr_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50),
-                        cancer_fn=dict(ld50=thisdf.ohr_cancer_fn_ld50.iloc[0], method='cin_integral', **dict(k=thisdf.ohr_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50)),
-                        dur_cin=dict(dist='lognormal', par1=thisdf.ohr_dur_cin_par1.iloc[0], par2=thisdf.ohr_dur_cin_par2.iloc[0]),
-                        rel_beta=thisdf.ohr_rel_beta.iloc[0]
-                    )
-                )
-            )
-            if ~np.isnan(thisdf.cross_imm_sev_high.iloc[0]):
-                sc_pars[location]['cross_imm_sev_high'] = thisdf.cross_imm_sev_high.iloc[0]
-                sc_pars[location]['cross_imm_sev_med'] = thisdf.cross_imm_sev_med.iloc[0]
-                sc_pars[location]['cross_imm_sus_high'] = thisdf.cross_imm_sus_high.iloc[0]
-                sc_pars[location]['cross_imm_sus_med'] = thisdf.cross_imm_sus_med.iloc[0]
+        # alldf = sc.loadobj('results/calib_dfs_sc.obj')
+        # sc_pars = dict()
+        # for location in locations:
+        #     thisdf = alldf[alldf.location == location]
+        #     sc_pars[location] = dict(
+        #         genotype_pars=dict(
+        #             hi5=dict(
+        #                 cin_fn=dict(k=thisdf.hi5_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50),
+        #                 cancer_fn=dict(ld50=thisdf.hi5_cancer_fn_ld50.iloc[0], method='cin_integral', **dict(k=thisdf.hi5_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50)),
+        #                 dur_cin=dict(dist='lognormal', par1=thisdf.hi5_dur_cin_par1.iloc[0], par2=thisdf.hi5_dur_cin_par2.iloc[0]),
+        #                 rel_beta=thisdf.hi5_rel_beta.iloc[0]
+        #             ),
+        #             ohr=dict(
+        #                 cin_fn=dict(k=thisdf.ohr_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50),
+        #                 cancer_fn=dict(ld50=thisdf.ohr_cancer_fn_ld50.iloc[0], method='cin_integral', **dict(k=thisdf.ohr_cin_fn_k.iloc[0], form='logf2', x_infl=0, ttc=50)),
+        #                 dur_cin=dict(dist='lognormal', par1=thisdf.ohr_dur_cin_par1.iloc[0], par2=thisdf.ohr_dur_cin_par2.iloc[0]),
+        #                 rel_beta=thisdf.ohr_rel_beta.iloc[0]
+        #             )
+        #         )
+        #     )
+        #     if ~np.isnan(thisdf.cross_imm_sev_high.iloc[0]):
+        #         sc_pars[location]['cross_imm_sev_high'] = thisdf.cross_imm_sev_high.iloc[0]
+        #         sc_pars[location]['cross_imm_sev_med'] = thisdf.cross_imm_sev_med.iloc[0]
+        #         sc_pars[location]['cross_imm_sus_high'] = thisdf.cross_imm_sus_high.iloc[0]
+        #         sc_pars[location]['cross_imm_sus_med'] = thisdf.cross_imm_sus_med.iloc[0]
 
-        sims, calib = run_calib(locations=locations, sc_pars=sc_pars, n_trials=n_trials, n_workers=n_workers, do_save=do_save, do_plot=False, filestem=filestem)
 
     # Load the calibration, plot it, and save the best parameters -- usually locally
     if 'plot_calibration' in to_run:
